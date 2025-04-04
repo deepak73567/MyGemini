e6f2c2d538552b21e1a4c7fd496623552ba7b0c9
Deepak Vishwakarma <735deepak.v@gmail.com>
2025-04-04 12:03:00 +0530

    demo

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+app.use(bodyParser.json());
+
+app.post('/getResponse', async (req, res) => {
+    console.log("Received question:", req.body.question);
+
+    const genAI = new GoogleGenerativeAI('AIzaSyAgbBMGxHtGH18EsH3VThHKrNFoJZB7vCk'); // Replace with your API key
+    const model=genAI.getGenerativeModel({model:"gemini-1.5-flash"});
+
+  model.generateContent(req.body.question).then(result=>{
+   console.log(result.response.text());
+   const response=result.response.text();
+   res.status(200).json({
+    response:response
+   })
+  }) .catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        error:err
+    })
+  })
+
+    
+});
+module.exports = app;
