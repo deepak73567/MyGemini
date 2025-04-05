39fd18773f8be4b139c78818b1cd3874b759690f
Deepak Vishwakarma <735deepak.v@gmail.com>
2025-04-05 14:35:08 +0530

    demo

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
+const cors=require('cors')
 app.use(bodyParser.json());
+
+app.use(cors({
+  origin: 'https://my-gemini-eta-nine.vercel.app', // Ya jo bhi tumhara frontend domain ho
+  methods: ['GET', 'POST'],
+  credentials: true // Agar cookies ya headers use kar rahe ho toh
+}));
+
 
 app.post('/getResponse', async (req, res) => {
     console.log("Received question:", req.body.question);
